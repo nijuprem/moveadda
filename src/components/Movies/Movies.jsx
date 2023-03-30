aa29f25e2f160f8f45a0f8805282bac018be6845
niju.prem <nijuprem7@gmail.com>
2023-03-30 13:52:47 +0530

    Corrected some responsiveness isses on lists

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -11,6 +11,8 @@ const Movies = () => {
   const { genreIdOrCategoryName, searchQuery } = useSelector((state)=> state.currentGenreOrCategory);
   const { data, error, isFetching } = useGetMoviesQuery({genreIdOrCategoryName, page, searchQuery});
   // console.log(data)
+  const lg= useMediaQuery((theme)=>theme.breakpoints.only('lg'));
+  const numberOfMovies = lg ? 16 : 18; 
 
   if(isFetching){
     return( 
@@ -35,7 +37,7 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies={data} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages} />
     </div>
   )
