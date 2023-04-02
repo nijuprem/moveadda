c13401d9bbda610b25dd7f6d9fd3100c225861a2
niju.prem <nijuprem7@gmail.com>
2023-04-02 19:18:20 +0530

    Added few features for alan ai

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,27 +1,57 @@
 import React, { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import { ColorModeContext } from "../utils/ToggleColorMode";
+import { fetchToken } from "../utils/index";
+import {
+    searchMovie,
+    selectGenreOrCategory,
+} from "../features/currentGenreOrCategory";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 
 const useAlan = () => {
     const { setMode } = useContext(ColorModeContext);
-
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
         alanBtn({
-            key: "aa44334b0d8df7afa8d2dde33247a6562e956eca572e1d8b807a3e2338fdd0dc/stage",
-            onCommand: ({ command, mode }) => {
-                if (command === "changeMode") {
+            key: "c140bdcb0dc6d1262ec5b4d0f7c942fe2e956eca572e1d8b807a3e2338fdd0dc/stage",
+            onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+                if (command === "chooseGenre") {
+                    const foundGenre = genres.find(
+                        (g) =>
+                            g.name.toLowerCase() ===
+                            genreOrCategory.toLowerCase()
+                    );
+                    if (foundGenre) {
+                        navigate("/");
+                        dispatch(selectGenreOrCategory(foundGenre.id));
+                    } else {
+                        const category = genreOrCategory.startsWith("top")
+                            ? "top_rated"
+                            : genreOrCategory;
+                        navigate("/");
+                        dispatch(selectGenreOrCategory(category));
+                    }
+                } else if (command === "changeMode") {
                     if (mode === "light") {
                         setMode("light");
                     } else {
                         setMode("dark");
                     }
-                } 
+                } else if (command === "login") {
+                    fetchToken();
+                } else if (command === "logout") {
+                    localStorage.clear();
+                    navigate("/");
+                } else if (command === "search") {
+                    dispatch(searchMovie(query));
+                }
             },
         });
     }, []);
-}; 
+};
 
- 
-export default useAlan
+export default useAlan;
