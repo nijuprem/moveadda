fa06b4007abe33b7cf5cfe2aafa1133ea2a359a9
niju.prem <nijuprem7@gmail.com>
2023-04-05 10:18:20 +0530

    Added featured movie component

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,13 +4,14 @@ import { Grid } from '@mui/material';
 import useStlyes from './styles'
 import Movie from '../Movie/Movie';
 
-const MovieList = ({movies, numberOfMovies}) => {
+const MovieList = ({movies, numberOfMovies, excludeFirst}) => {
 
     const classes= useStlyes();
+    const startFrom = excludeFirst? 1 : 0;
 
   return (
     <Grid container className={classes.moviesContainer}>
-      {movies.results.slice(0, numberOfMovies).map((movie, i)=>(
+      {movies.results.slice(startFrom, numberOfMovies).map((movie, i)=>(
         <Movie key={i} i={i} movie={movie} />
       ))}
     </Grid>
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,18 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {Box, CircularProgress, useMediaQuery, Typography} from '@mui/material';
 import { useSelector } from 'react-redux';
-import {useGetMoviesQuery} from '../../services/TMDB'
-import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import {useGetMoviesQuery} from '../../services/TMDB';
 import MovieList from '../MovieList/MovieList';
 import Pagination from '../Pagination/Pagination'
+import FeaturedMovie from '../FeaturedMovie/FeaturedMovie';
 
 const Movies = () => {
   const [page, setPage] = useState(1);
   const { genreIdOrCategoryName, searchQuery } = useSelector((state)=> state.currentGenreOrCategory);
   const { data, error, isFetching } = useGetMoviesQuery({genreIdOrCategoryName, page, searchQuery});
-  // console.log(data)
+  // console.log(data?.results[0])
   const lg= useMediaQuery((theme)=>theme.breakpoints.only('lg'));
-  const numberOfMovies = lg ? 16 : 18; 
+  const numberOfMovies = lg ? 17 : 19; 
 
   if(isFetching){
     return( 
@@ -37,7 +37,8 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies={data} numberOfMovies={numberOfMovies} />
+    <FeaturedMovie movie={data?.results[0]}/>
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages} />
     </div>
   )
